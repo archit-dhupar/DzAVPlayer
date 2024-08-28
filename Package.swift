ea8f1fcf733d8c644d036c04db6425b819601c17
archit-dhupar <archit.dhupar@gmail.com>
2024-08-28 17:37:39 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "DzBase", url: "https://github.com/archit-dhupar/DzBase.git", .upToNextMajor(from: "0.0.1"))
     ],
     targets: [
       
@@ -22,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DzAVPlayerAdapter",
-            url: "https://github.com/archit-dhupar/DzAVPlayer/releases/download/0.0.7/DzAVPlayerAdapter.xcframework.zip",
+            dependencies: ["DzBase"],
+            url: "https://github.com/archit-dhupar/DzAVPlayer/releases/download/0.0.8/DzAVPlayerAdapter.xcframework.zip",
             checksum: "77d541df36c2e572c41fe975d4cd4ed6c4c6442e57505676f308b56a79649edd"),
     ],
     swiftLanguageVersions: [.v5]
