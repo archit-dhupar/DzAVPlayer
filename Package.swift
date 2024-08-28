e681f6ad939c93bc839ff121dc29a9002e4d08d4
archit-dhupar <archit.dhupar@gmail.com>
2024-08-28 15:16:10 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DzAVPlayerAdapter",
-            targets: ["DzAVPlayerAdapter"]),
+            name: "AVPlayerAdapter",
+            targets: ["AVPlayerAdapter"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,8 +21,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "DzAVPlayerAdapter",
-            url: "https://github.com/archit-dhupar/DzAVPlayer/releases/download/0.0.2/DzAVPlayerAdapter.xcframework.zip",
+            name: "AVPlayerAdapter",
+            url: "https://github.com/archit-dhupar/DzAVPlayer/releases/download/0.0.3/AVPlayerAdapter.xcframework.zip",
             checksum: "2e45a36e413dd408994430defd9d6f75463e0d832688b590b67697cd750c7c58"),
     ],
     swiftLanguageVersions: [.v5]
